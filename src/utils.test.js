8188ad13e2efc71531938f74e96aa37fb9ff624c
Carlos Santisteban <Carlos Santisteban>
2017-07-09 19:34:45 -0500

    added matchQuery util tests

--- src/utils.test.js
+++ src/utils.test.js
@@ -1,4 +1,4 @@
-import { getMatchSubstrings } from './utils.js'
+import { getMatchSubstrings, matchQuery } from './utils.js'
 
 describe('getMatchSubstrings util', function () {
 
@@ -8,18 +8,47 @@ describe('getMatchSubstrings util', function () {
     expect(object).toEqual(subject);
   });
 
-  test('test for empty query argument"', function () {
+  test('empty query argument"', function () {
     const object = getMatchSubstrings("string here", "");
     expect(object).toEqual([]);
   });
 
-  test('test for empty string argument"', function () {
+  test('empty string argument"', function () {
     const object = getMatchSubstrings("", "query");
     expect(object).toEqual([]);
   });
 
-  test('test for undefined query argument"', function () {
+  test('undefined query argument"', function () {
     const object = getMatchSubstrings("something", undefined);
+    expect(object).toEqual([]);
+  });
+
+});
+
+describe('matchQuery util', function () {
+
+  const colors = ['red', 'blue', 'yellow', 'dark-blue', 'light-blue', 'pink', 'green', 'light-green', 'purple', 'brown', 'yellow', 'orange']
+
+  test('find query "blue" in an array of colors"', function () {
+    const object = matchQuery(colors, "blue");
+    const subject = [{ title: 'blue', matchList: [{ length: 4, offset: 0 }], score: 0 },
+    { title: 'dark-blue', matchList: [{ length: 4, offset: 5 }], score: 5 / 4 * 9 },
+    { title: 'light-blue', matchList: [{ length: 4, offset: 6 }], score: 6 / 4 * 10 }];
+    expect(object).toEqual(subject);
+  });
+
+  test('empty query argument"', function () {
+    const object = matchQuery(colors, "");
+    expect(object).toEqual([]);
+  });
+
+  test('empty array argument"', function () {
+    const object = getMatchSubstrings([], "query");
+    expect(object).toEqual([]);
+  });
+
+  test('undefined query argument"', function () {
+    const object = getMatchSubstrings(colors, undefined);
     expect(object).toEqual([]);
   });
 
